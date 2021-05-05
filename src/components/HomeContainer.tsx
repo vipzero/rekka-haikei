a5ddce32e313986e8450f44e431bbc261fb11e9c
vipzero <hiroflighter@gmail.com>
2021-05-05 14:26:37 +0900

    fix extra button

--- src/components/HomeContainer.tsx
+++ src/components/HomeContainer.tsx
@@ -8,13 +8,13 @@ import { useSongDb } from './useSongDb'
 function getEx(ex: string | false) {
 	if (ex === 'nonnon') {
 		return (
-			<div style={{ height: '100%' }}>
+			<div style={{ height: '50vh' }}>
 				<iframe style={{ width: '100%' }} src="https://nyanpass.com/" />
 			</div>
 		)
 	} else if (ex === 'mia') {
 		return (
-			<div style={{ height: '100%' }}>
+			<div style={{ height: '50vh' }}>
 				<iframe
 					style={{ width: '100%', height: '100%' }}
 					src="https://click.abyss.fun/"
@@ -22,6 +22,7 @@ function getEx(ex: string | false) {
 			</div>
 		)
 	}
+	return null
 }
 
 function HomeContainer() {
@@ -31,12 +32,13 @@ function HomeContainer() {
 	const [lyrics] = useLyricsDb(song.icy, showLyrics)
 
 	useEffect(() => {
-		if (isSongFull(song)) {
-			if (song.animeTitle.includes('のんのんびより')) {
-				setEx('nonnon')
-			} else if (song.animeTitle.includes('メイドインアビス')) {
-				setEx('mia')
-			}
+		if (!isSongFull(song)) {
+			return setEx(false)
+		}
+		if (song.animeTitle.includes('のんのんびより')) {
+			setEx('nonnon')
+		} else if (song.animeTitle.includes('修羅場')) {
+			setEx('mia')
 		} else {
 			setEx(false)
 		}
--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 const Error = () => {
 	return (
 		<Layout title="404">
-			<h1>そんなページはないぞ</h1>
+			<h2>エラーorz</h2>
 		</Layout>
 	)
 }
