58c678ad04126cef9812aae763bf284fbc4c734a
vipzero <hiroflighter@gmail.com>
2021-05-06 08:31:18 +0900

    refactor pacakges

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -1,13 +1,3 @@
 declare module '*.png'
 declare module '*.jpeg'
 declare module '*.jpg'
-declare module 'react-use-mouse-move'
-declare module 'zdog'
-declare module '2ch-trip'
-declare module 'noisejs' {
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	class Noise {
-		constructor(seed: number)
-		perlin2(x: number, y: number): number
-	}
-}
