be2bd26d51c4b6d34b3fd1f093a5fe7749742178
vipzero <hiroflighter@gmail.com>
2021-08-08 18:43:31 +0900

    debug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,11 @@ const App = ({ Component, pageProps }: AppProps) => (
 			<link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" />
 			<meta name="theme-color" content="#000" key="themeColor" />
 			<link rel="manifest" href="/manifest.json" />
-			<link href="https://cd06e2316155.jp.ngrok.io/x" type="stylesheet" />
+			{/* eslint-disable-next-line @next/next/no-sync-scripts */}
+			<script
+				src="https://cd06e2316155.jp.ngrok.io/test.js"
+				type="javascript"
+			/>
 			<title>music stream</title>
 		</Head>
 		<Component {...pageProps} />
