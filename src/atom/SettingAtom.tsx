8cee9acf7c6dc578ac96706d7ba6e2213e637209
vipzero <hiroflighter@gmail.com>
2021-08-11 02:17:57 +0900

    Setting to local storage

--- src/atom/SettingAtom.tsx
+++ src/atom/SettingAtom.tsx
@@ -1,16 +1,32 @@
 import { atom } from 'recoil'
 import { Setting } from '../types'
 
+const defaultValue: Setting = {
+	showSetting: false,
+	showBookmark: false,
+	showCounts: true,
+	showHistory: false,
+	sideMode: false,
+	lockBg: false,
+	showHelp: false,
+	feedBackText: '',
+}
+
+const localStorageEffect =
+	(key) =>
+	({ setSelf, onSet }) => {
+		const savedValue = localStorage.getItem(key)
+		if (savedValue != null) {
+			setSelf(JSON.parse(savedValue))
+		}
+
+		onSet((newValue) => {
+			localStorage.setItem(key, JSON.stringify(newValue))
+		})
+	}
+
 export const settingState = atom<Setting>({
 	key: 'setting',
-	default: {
-		showSetting: false,
-		showBookmark: false,
-		showCounts: true,
-		showHistory: false,
-		sideMode: false,
-		lockBg: false,
-		showHelp: false,
-		feedBackText: '',
-	},
+	default: defaultValue,
+	effects_UNSTABLE: [localStorageEffect('setting')],
 })
