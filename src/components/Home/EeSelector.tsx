76ded662a16552c8045ccf1e58bd49cc8f97fc17
vipzero <hiroflighter@gmail.com>
2022-04-12 20:38:45 +0900

    add hover eekey

--- src/components/Home/EeSelector.tsx
+++ src/components/Home/EeSelector.tsx
@@ -15,12 +15,16 @@ export const EeSelector = (props: Props) => {
 			{eeSaw.map((b, i) => (
 				<div
 					key={i}
+					className="tooltip"
 					data-active={eeKey === eekeys[i]}
 					onClick={() => {
 						if (!b) return
 						setEekey(eekeys[i], true)
 					}}
 				>
+					<div className="tooltip-text">
+						{b ? eekeys[i] : eekeys[i].replace(/./g, '.')}
+					</div>
 					{b ? '*' : '-'}
 				</div>
 			))}
@@ -30,8 +34,10 @@ export const EeSelector = (props: Props) => {
 
 const Style = styled.div`
 	display: flex;
-	> div:not(.head) {
-		cursor: default;
+
+	.tooltip {
+		position: relative;
+		cursor: pointer;
 		&:hover {
 			background: gray;
 			filter: invert(1);
@@ -39,5 +45,28 @@ const Style = styled.div`
 		&[data-active='true'] {
 			background: orange;
 		}
+	}
+
+	.tooltip-text {
+		opacity: 0;
+		visibility: hidden;
+		position: absolute;
+		left: 50%;
+		transform: translateX(-50%);
+		bottom: -30px;
+		display: inline-block;
+		padding: 4px;
+		white-space: nowrap;
+		font-size: 0.8rem;
+		line-height: 1.3;
+		background: #333;
+		color: #fff;
+		border-radius: 4px;
+		transition: 0.3s ease-in;
+	}
+
+	.tooltip:hover .tooltip-text {
+		opacity: 1;
+		visibility: visible;
 	}
 `
