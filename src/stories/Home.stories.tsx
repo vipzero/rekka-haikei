d5929a97b7c493eeae3ea6bd0e2f81cb6603433d
vipzero <hiroflighter@gmail.com>
2022-04-26 22:16:13 +0900

    reefactor

--- src/stories/Home.stories.tsx
+++ src/stories/Home.stories.tsx
@@ -7,6 +7,7 @@ import { eekeys } from '../components/Home/Cvote/constants'
 import { GlobalStyle } from '../config/init'
 import { useSettings } from '../hooks/useSettings'
 import { Setting } from '../types'
+import { seedSong as song } from './seed'
 
 type Props = ComponentProps<typeof Home> & { setting: Setting }
 
@@ -16,30 +17,6 @@ const HomeRecoil = ({ setting, ...props }: Props) => {
 		setSetting(setting)
 	}, [])
 	return <Home {...props} />
-}
-
-const song = {
-	imageSearchWord: '普通の女子校生が【ろこどる】やってみた。',
-	writer: '滝澤俊輔(TRYTONELABO)',
-	artist: '宇佐美奈々子(CV.伊藤美来)・小日向縁(CV.三澤紗千香)',
-	arranger: '滝澤俊輔',
-	composer: '桜アス恵',
-	animeTitle: '普通の女子校生が【ろこどる】やってみた。',
-	title: '流川ガールズソング ',
-	imageLinks: [
-		'https://cs1.anime.dmkt-sp.jp/anime_kv/img/11/37/1/11371_1_9_8b.png?1551176652000',
-	],
-	icy: '宇佐美奈々子(CV.伊藤美来)・小日向縁(CV.三澤紗千香) - 流川ガールズソング ',
-	singer: '宇佐美奈々子(伊藤美来)/小日向縁(三澤紗千香)',
-	time: 1641221903552,
-	wordCounts: { 三澤紗千香: 4, 伊藤美来: 23, 宇佐美奈々子: 4, 小日向縁: 4 },
-	wordCountsAna: [
-		{ count: 1, label: '[流川ガールズソング  (初)]', name: '流川ガールズ' },
-		{ count: 4, label: '[三澤紗千香 (4回目)]', name: '三澤紗千香' },
-		{ count: 4, label: '[宇佐美奈々子 (4回目)]', name: '宇佐美奈々子' },
-		{ count: 4, label: '[小日向縁 (4回目)]', name: '小日向縁' },
-		{ count: 23, label: '[伊藤美来 (23回目)]', name: '伊藤美来' },
-	],
 }
 
 export default {
@@ -69,9 +46,7 @@ export default {
 			query: { id: '2000winter' },
 		},
 	},
-	args: {
-		song,
-	},
+	args: { song },
 } as ComponentMeta<typeof Home>
 
 const Template: ComponentStory<typeof HomeRecoil> = (args) => (
