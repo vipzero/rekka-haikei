49691ac292ff69b3e8a639d89e8b2443fc14a5a0
vipzero <hiroflighter@gmail.com>
2022-04-29 07:46:33 +0900

    update note

--- src/components/HistoryPage/Address.tsx
+++ src/components/HistoryPage/Address.tsx
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 import { EventLinks } from '../EventLinks'
 
 const notes: Record<string, string> = {
-	'2022-04-26:19': 'feat PWA',
+	'2022-04-29:01': 'feat 回転許可',
+	'2022-04-29:00': 'feat アートワーク非表示ボタン',
+	'2022-04-26:19': 'feat アプリ化',
 	'2022-04-12:19': 'feat 投票の追加分表示',
 	'2022-04-12:17': 'feat 履歴移動バグ修正',
 	'2022-01-01:02': 'feat ホバーでプレイヤー表示',
