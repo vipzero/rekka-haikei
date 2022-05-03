5abfce8e55191212aa2bfadd1bbab96d511dd5bc
vipzero <hiroflighter@gmail.com>
2022-05-03 16:59:17 +0900

    add help text

--- src/components/Home/BookCount.tsx
+++ src/components/Home/BookCount.tsx
@@ -4,6 +4,13 @@ type Props = { songId: number }
 const BookCount = ({ songId }: Props) => {
 	const { bookCount } = useBookCountDb(songId)
 
-	return <p style={{ paddingTop: 0 }}>★:{bookCount}</p>
+	return (
+		<p style={{ paddingTop: 0 }}>
+			<span className="tooltip">
+				<span className="tooltip-text">ブックマークされた回数</span>
+				★:{bookCount}
+			</span>
+		</p>
+	)
 }
 export default BookCount
