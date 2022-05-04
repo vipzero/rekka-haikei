e81e58c31e561d5f64efb3e1946dc35505df088e
vipzero <hiroflighter@gmail.com>
2022-05-04 15:55:11 +0900

    add style preview

--- src/components/HistoryPage/SettingPage.tsx
+++ src/components/HistoryPage/SettingPage.tsx
@@ -5,6 +5,7 @@ import {
 	useSettingsFakeBar,
 } from '../../hooks/useSettings'
 import { CheckBox } from '../common/CheckBox'
+import ToggleButton, { ConfButton } from '../Home/ToggleButton'
 
 type Props = {}
 
@@ -13,7 +14,7 @@ export function SettingPage() {
 	const { customTheme, setCustomTheme } = useSettingsCustomTheme()
 
 	return (
-		<Style>
+		<Style customTheme={customTheme}>
 			<div style={{ width: 'max-content' }}>
 				<h4>オプション</h4>
 				<CheckBox
@@ -37,13 +38,84 @@ export function SettingPage() {
 					onChange={(e) => setCustomTheme(e.target.value)}
 				/>
 			</div>
+			<div className="preview">
+				<div id="timebar">
+					<div className="wrap">
+						<div className="fill" style={{ width: '30%' }} />
+						<div className="pointer" />
+					</div>
+				</div>
+				<div id="panel">
+					<p>タイトル</p>
+				</div>
+				<div id="setting-box">
+					<ToggleButton onClick={() => {}} checked>
+						ON
+					</ToggleButton>
+					<ToggleButton onClick={() => {}} checked={false}>
+						OFF
+					</ToggleButton>
+				</div>
+			</div>
 		</Style>
 	)
 }
-const Style = styled.div`
+const Style = styled.div<{ customTheme: string }>`
 	.custom-theme-code {
 		width: max-content;
 
 		font-family: 'Roboto Mono', monospace;
+	}
+	${(p) => p.customTheme}
+	.preview {
+		background: url('/static/pattern-randomized.svg');
+		width: 400px;
+		border: solid 1px;
+		.wrap {
+			display: flex;
+			position: absolute;
+			width: 400px;
+			height: 4px;
+			background: #222;
+		}
+		.fill {
+			background: #ddd;
+			height: 4px;
+		}
+
+		#panel {
+			background-color: var(--content-bg-color);
+			padding: 4px;
+			margin: 4px;
+		}
+		#setting-box {
+			padding: 8px;
+			background: var(--setting-bg-color);
+		}
+
+		button {
+			border-radius: 4px;
+			color: var(--btn-fo-color);
+			background-color: var(--btn-bg-color);
+			&[data-checked='true'] {
+				background: var(--btn-bg-checked-color) !important;
+			}
+		}
+		#panel,
+		[data-co] {
+			p,
+			a {
+				font-weight: bold;
+				color: var(--panel-fo-color);
+				text-shadow: 1px 1px 1px var(--panel-fo-shadow-color),
+					-1px -1px 1px var(--panel-fo-shadow-color),
+					-1px 1px 1px var(--panel-fo-shadow-color),
+					1px -1px 1px var(--panel-fo-shadow-color);
+				margin: 0;
+			}
+		}
+		.typography {
+			color: var(--font-color);
+		}
 	}
 `
