53e3bec46cc9f0fd211a8ac0ebba6825e3a3207d
vipzero <hiroflighter@gmail.com>
2022-11-20 16:37:54 +0900

    add Hour snap sotry

--- src/stories/SnapReplica.stories.tsx
+++ src/stories/SnapReplica.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { range } from 'lodash'
 import { ComponentProps } from 'react'
 import { SnapReplica } from '../components/BookPage/SnapReplica'
 import { GlobalStyle } from '../config/init'
@@ -36,3 +37,27 @@ export const Snap2 = Template.bind({})
 Snap2.args = {
 	snap: snaps[1],
 }
+
+const Cols = {}
+
+range(24).forEach((v) => {
+	const Col = Template.bind({})
+	Col.args = {
+		snap: { ...snaps[1], animeTitle: `${v}時`, time: (v - 9) * 60 * 60 * 1000 },
+	}
+	Cols[`Hour${v}`] = Col
+})
+
+export const Cols0 = Cols['Hour0']
+export const Cols3 = Cols['Hour3']
+export const Cols6 = Cols['Hour6']
+export const Cols9 = Cols['Hour9']
+export const Cols12 = Cols['Hour12']
+export const Cols15 = Cols['Hour15']
+export const Cols18 = Cols['Hour18']
+export const Cols21 = Cols['Hour21']
+
+// export const Col1 = Template.bind({})
+// Col1.args = {
+// 	snap: { ...snaps[1], time: 0 },
+// }
