c1a3017f56ef726d1668eda65b893f1b2c12b972
vipzero <hiroflighter@gmail.com>
2022-12-17 22:09:31 +0900

    fix ee

--- src/components/Home/Cvote/constants.ts
+++ src/components/Home/Cvote/constants.ts
@@ -146,4 +146,5 @@ export const EX_PATTERNS_ICY: [string | RegExp, Eekey][] = [
 	[/color|虹|rainbow/i, 'rainbow'],
 	[/雨([^上宮]|!?あがり)|[^a-z]rain/i, 'rain'],
 	[/�/, 'lain'],
+	[/・高嶺 ?愛花|小早川 ?凛子|姉ヶ崎 ?寧々/, 'loveplus'],
 ]
