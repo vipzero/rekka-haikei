25f41e1cf5844f83515555f46187a2959b22058a
vipzero <hiroflighter@gmail.com>
2022-12-21 20:32:06 +0900

    fix filter

--- src/config/SvgFilters.tsx
+++ src/config/SvgFilters.tsx
@@ -3,10 +3,16 @@ import React from 'react'
 export const SvgFilters = () => (
 	<>
 		<svg style={{ display: 'none' }}>
-			<filter id="wavy">
+			<filter
+				id="wavy"
+				x="-100%"
+				y="-100%"
+				width="400%"
+				height="400%"
+				filterUnits="objectBoundingBox"
+			>
 				<feTurbulence
-					x="-100"
-					y="-100"
+					// type="fractalNoise"
 					baseFrequency="0.009"
 					numOctaves="5"
 					seed="2"
@@ -14,11 +20,11 @@ export const SvgFilters = () => (
 					<animate
 						attributeName="baseFrequency"
 						dur="60s"
-						values="0.02;0.005;"
-						rotate="90"
+						values="0.01;0.020;0.01"
 						repeatCount="indefinite"
 					/>
 				</feTurbulence>
+				<feOffset dx="500" dy="100" in="BackgroundImage" />
 				<feDisplacementMap in="SourceGraphic" scale="30" />
 			</filter>
 		</svg>
