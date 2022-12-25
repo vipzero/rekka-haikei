ef84cd545e17b077602b7e2f1b6b572c22df075a
vipzero <hiroflighter@gmail.com>
2022-12-25 12:33:06 +0900

    fix modal scale

--- src/components/BgChoise/BgChoice.tsx
+++ src/components/BgChoise/BgChoice.tsx
@@ -34,15 +34,19 @@ const Wrap = styled.div`
 	padding: 16px;
 	display: flex;
 	flex-wrap: wrap;
-	width: 200%;
-	transform: scale(0.5, 0.5);
-	transform-origin: top left;
+	width: 100%;
 
 	div {
 		margin: 4px;
 		padding: 2px;
 		border: solid black 1px;
 		cursor: pointer;
+
+		zoom: 0.4;
+		max-width: 1000px;
+		@media only screen and (max-width: 600px) {
+			zoom: 0.2;
+		}
 
 		&:first-child {
 			border-color: red;
@@ -53,7 +57,8 @@ const Wrap = styled.div`
 		}
 	}
 	img {
-		min-width: 100px;
+		/* transform-origin: top left; */
+		width: 100%;
 		max-width: 1000px;
 		@media only screen and (max-width: 600px) {
 			max-width: min(1000px, 100%);
